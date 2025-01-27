6805f816f64c18c22a0f402561a5c2075391126c
furkancanc <fcc.ceylan@gmail.com>
2025-01-27 22:11:10 +0300

    Controlling Date With A Joystick

--- Assets/Kawaii Survivor/Scripts/PlayerController.cs
+++ Assets/Kawaii Survivor/Scripts/PlayerController.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private MobileJoystick playerJoystick;
+    [SerializeField] private float moveSpeed;
     private Rigidbody2D rig;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        rig.linearVelocity = Vector2.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rig.linearVelocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
 }
