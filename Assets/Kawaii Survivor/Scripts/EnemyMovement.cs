69b8f1bf3db021c282444e11d7ce7d36120ada68
furkancanc <fcc.ceylan@gmail.com>
2025-01-28 23:23:11 +0300

    Following The Player

---
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Elements")]
+    private Player player;
+
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+
+        if (player == null)
+        {
+            Debug.Log("No player found, Auto-destroying.");
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+
+        transform.position = targetPosition;
+    }
+}
