c8b9d999649bb430a7fbc64bfa6a8876531f9f11
furkancanc <fcc.ceylan@gmail.com>
2025-01-29 23:04:17 +0300

    Adding A Health Bar

--- Assets/Kawaii Survivor/Scripts/Player/PlayerHealth.cs
+++ Assets/Kawaii Survivor/Scripts/Player/PlayerHealth.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    private int health;
+
+    private void Start()
+    {
+        health = maxHealth;
+        Debug.Log("Health initialized to: " + health);
+    }
 
     public void TakeDamage(int damage)
     {
         int realDamage = Mathf.Min(damage, health);
         health -= damage;
+
+        Debug.Log("Health : " + health);
 
         if (health <= 0)
         {
