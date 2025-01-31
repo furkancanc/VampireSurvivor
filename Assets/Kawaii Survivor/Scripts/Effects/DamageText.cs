4061f9f05a9caaf9c6cf03edbc4ccb6f7685c63e
furkancanc <fcc.ceylan@gmail.com>
2025-01-31 21:28:03 +0300

    The First Particle Animation

---
+++ Assets/Kawaii Survivor/Scripts/Effects/DamageText.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class DamageText : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private TextMeshPro damageText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [NaughtyAttributes.Button]
+    private void Animate()
+    {
+        damageText.text = Random.Range(10, 1000).ToString();
+        animator.Play("Animate");
+    }
+}
