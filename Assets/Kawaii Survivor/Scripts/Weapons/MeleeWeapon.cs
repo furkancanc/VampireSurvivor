1cab08ed94c77d0ba89ae2e9b029457d8b3f89c9
furkancanc <fcc.ceylan@gmail.com>
2025-02-02 18:12:25 +0300

    Range Weapon Is Working

--- Assets/Kawaii Survivor/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Kawaii Survivor/Scripts/Weapons/MeleeWeapon.cs
@@ -1,16 +1,111 @@
 using UnityEngine;
 
-public class MeleeWeapon : MonoBehaviour
+public class MeleeWeapon : Weapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum State
     {
-        
+        Idle,
+        Attack
     }
 
-    // Update is called once per frame
-    void Update()
+    private State state;
+
+    [Header("Elements")]
+    [SerializeField] private Transform hitDetectionTransform;
+    [SerializeField] private BoxCollider2D hitCollider;
+    
+    private void Start()
     {
-        
+        state = State.Idle;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                AutoAim();
+                break;
+            case State.Attack:
+                Attacking();
+                break;
+        }
+    }
+
+    private void AutoAim()
+    {
+        Enemy closestEnemy = GetClosestEnemy();
+        Vector2 targetUpVector = Vector3.up;
+
+        if (closestEnemy != null)
+        {
+            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+            transform.up = targetUpVector;
+            ManageAttackTimer();
+        }
+
+        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+        IncrementAttackTimer();
+    }
+
+    private void ManageAttackTimer()
+    {
+        if (attackTimer >= attackDelay)
+        {
+            attackTimer = 0;
+            StartAttack();
+        }
+    }
+
+    private void IncrementAttackTimer()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+    [NaughtyAttributes.Button]
+    private void StartAttack()
+    {
+        animator.Play("Attack");
+        state = State.Attack;
+
+        damagedEnemies.Clear();
+
+        animator.speed = 1f / attackDelay;
+    }
+
+    private void Attacking()
+    {
+        Attack();
+    }
+
+    private void StopAttack()
+    {
+        state = State.Idle;
+
+        // Clear the attacked enemies
+        // Damaged enemies list
+        damagedEnemies.Clear();
+    }
+
+    private void Attack()
+    {
+        //Collider2D[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask);
+        Collider2D[] enemies = Physics2D.OverlapBoxAll(hitDetectionTransform.position,
+            hitCollider.bounds.size,
+            hitDetectionTransform.localEulerAngles.z,
+            enemyMask
+            );
+
+        for (int i = 0; i < enemies.Length; ++i)
+        {
+            Enemy enemy = enemies[i].GetComponent<Enemy>();
+            // 1. Is the enemy inside of the list?
+            if (!damagedEnemies.Contains(enemy))
+            {
+                Debug.Log(damage);
+                enemy.TakeDamage(damage);
+                damagedEnemies.Add(enemy);
+            }
+        }
     }
 }
--- Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
+++ Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
@@ -2,32 +2,19 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
-    enum State 
-    { 
-        Idle,
-        Attack
-    }
-
-    private State state;
-
-    [Header("Elements")]
-    [SerializeField] private Transform hitDetectionTransform;
-    [SerializeField] private BoxCollider2D hitCollider;
-    [SerializeField] private float hitDetectionRadius;
-
     [Header("Settings")]
     [SerializeField] private float range;
-    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] protected LayerMask enemyMask;
 
     [Header("Attack")]
     [SerializeField] protected int damage;
     [SerializeField] protected float attackDelay;
-    [SerializeField] private Animator animator;
+    [SerializeField] protected Animator animator;
 
     protected float attackTimer;
-    private List<Enemy> damagedEnemies = new List<Enemy>();
+    protected List<Enemy> damagedEnemies = new List<Enemy>();
 
     [Header("Animations")]
     [SerializeField] protected float aimLerp;
@@ -35,101 +22,13 @@ public class Weapon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        state = State.Idle;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (state)
-        {
-            case State.Idle:
-                AutoAim();
-                break;
-            case State.Attack:
-                Attacking();
-                break;
-        }
-    }
-
-    private void AutoAim()
-    {
-        Enemy closestEnemy = GetClosestEnemy();
-        Vector2 targetUpVector = Vector3.up;
-
-        if (closestEnemy != null)
-        {
-            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
-            transform.up = targetUpVector;
-            ManageAttackTimer();
-        }
-
-        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
-        IncrementAttackTimer();
-    }
-
-    private void ManageAttackTimer()
-    {
-        if (attackTimer >= attackDelay)
-        {
-            attackTimer = 0;
-            StartAttack();
-        }
-    }
-
-    private void IncrementAttackTimer()
-    {
-        attackTimer += Time.deltaTime;
-    }
-
-    [NaughtyAttributes.Button]
-    private void StartAttack()
-    {
-        animator.Play("Attack");
-        state = State.Attack;
-
-        damagedEnemies.Clear();
-
-        animator.speed = 1f / attackDelay;
-    }
-
-    private void Attacking()
-    {
-        Attack();
-    }
-
-    private void StopAttack()
-    {
-        state = State.Idle;
-
-        // Clear the attacked enemies
-        // Damaged enemies list
-        damagedEnemies.Clear();
-    }
-
-    private void Attack()
-    {
-        //Collider2D[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask);
-        Collider2D[] enemies = Physics2D.OverlapBoxAll(hitDetectionTransform.position, 
-            hitCollider.bounds.size,
-            hitDetectionTransform.localEulerAngles.z,
-            enemyMask
-            );
-
-        for (int i = 0; i < enemies.Length; ++i)
-        {
-            Enemy enemy = enemies[i].GetComponent<Enemy>();
-            // 1. Is the enemy inside of the list?
-            if (!damagedEnemies.Contains(enemy))
-            {
-                Debug.Log(damage);
-                enemy.TakeDamage(damage);
-                damagedEnemies.Add(enemy);
-            }
-            // 2. If no, let's attack him, and add it to the list
-
-            // 3. If yes, let's continue, check the next enemy
-        }
+        
     }
 
     protected Enemy GetClosestEnemy()
@@ -164,13 +63,5 @@ public class Weapon : MonoBehaviour
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(transform.position, range);
-
-        if (hitDetectionTransform == null)
-        {
-            return;
-        }
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(hitDetectionTransform.position, hitDetectionRadius);
     }
 }
