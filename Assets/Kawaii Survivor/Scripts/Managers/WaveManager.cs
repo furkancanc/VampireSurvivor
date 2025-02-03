8ddd120463487188ed25c2bd0e6e01753a45398e
furkancanc <fcc.ceylan@gmail.com>
2025-02-03 22:49:01 +0300

    Introduction To Wave Manager

---
+++ Assets/Kawaii Survivor/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private Wave[] wave;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public struct Wave
+{
+    public string name;
+    public List<WaveSegment> segments;
+}
+
+[System.Serializable]
+public struct WaveSegment
+{
+    public GameObject prefab;
+}
