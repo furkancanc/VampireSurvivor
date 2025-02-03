48c1c2811b61eb8beb1e3baa3f8bc38815faac3b
furkancanc <fcc.ceylan@gmail.com>
2025-02-03 23:17:33 +0300

    The First Wave

--- Assets/Kawaii Survivor/Scripts/Managers/WaveManager.cs
+++ Assets/Kawaii Survivor/Scripts/Managers/WaveManager.cs
@@ -7,20 +7,55 @@ public class WaveManager : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float waveDuration;
+    private float timer;
 
     [Header("Waves")]
-    [SerializeField] private Wave[] wave;
+    [SerializeField] private Wave[] waves;
+    private List<float> localCounters = new List<float>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        localCounters.Add(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < waveDuration)
+        {
+            ManageCurrentWave();
+        }
+    }
+
+    private void ManageCurrentWave()
+    {
+        Wave currentWave = waves[0];
+
+        for (int i = 0; i < currentWave.segments.Count; ++i)
+        {
+            WaveSegment segment = currentWave.segments[i];
+
+            float tStart = segment.tStartEnd.x / 100 * waveDuration;
+            float tEnd   = segment.tStartEnd.y / 100 * waveDuration;
+
+            if (timer < tStart || timer > tEnd)
+            {
+                continue;
+            }
+
+            float timeSinceSegmentStart = timer - tStart;
+
+            float spawnDelay = 1f / segment.spawnFrequency;
+
+            if (timeSinceSegmentStart / spawnDelay > localCounters[i])
+            {
+                Instantiate(segment.prefab, Vector2.zero, Quaternion.identity, transform);
+                ++localCounters[i];
+            }
+        }
+
+        timer += Time.deltaTime;
     }
 }
 
