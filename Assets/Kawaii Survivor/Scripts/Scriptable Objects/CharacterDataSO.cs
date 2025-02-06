98319a25174a684ef9926db7caa59b21593a84a1
furkancanc <fcc.ceylan@gmail.com>
2025-02-06 22:12:27 +0300

    Scriptable Object

---
+++ Assets/Kawaii Survivor/Scripts/Scriptable Objects/CharacterDataSO.cs
@@ -0,0 +1,24 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character Data", menuName = "Scriptable Objects/New Character Data", order = 0)]
+public class CharacterDataSO : ScriptableObject
+{
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public Sprite Sprite { get; private set; }
+    [field: SerializeField] public int PurchasePrice { get; private set; }
+
+    [HorizontalLine]
+    [SerializeField] private float attack;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float criticalChance;
+    [SerializeField] private float criticalPercent;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float range;
+    [SerializeField] private float healthRecoverySpeed;
+    [SerializeField] private float armor;
+    [SerializeField] private float luck;
+    [SerializeField] private float ldodge;
+    [SerializeField] private float lifeSteal;
+}
