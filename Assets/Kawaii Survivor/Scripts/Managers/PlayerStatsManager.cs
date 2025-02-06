806fea978a5e1f401ee2889ab4cce89e00a2d0f1
furkancanc <fcc.ceylan@gmail.com>
2025-02-06 21:46:21 +0300

    PlayerStatsDependency interface

--- Assets/Kawaii Survivor/Scripts/Managers/PlayerStatsManager.cs
+++ Assets/Kawaii Survivor/Scripts/Managers/PlayerStatsManager.cs
@@ -1,11 +1,12 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerStatsManager : MonoBehaviour
 {
     [Header("Settings")]
-    private Dictionary<Stat, StatData> addens = new Dictionary<Stat, StatData>();
+    private Dictionary<Stat, float> addends = new Dictionary<Stat, float>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,46 +22,28 @@ public class PlayerStatsManager : MonoBehaviour
 
     public void AddPlayerStat(Stat stat, float value)
     {
-        //if (statDatas.ContainsKey(stat))
-        //{
-        //    statDatas[stat] += value;
-        //}
-        //else
-        //{
-        //    Debug.LogError($"The key {stat} has not been found, this is not normal behaviour!");
-        //}
-
-        StatData dataToAdd = new StatData(stat, value);
-        addens[stat] += dataToAdd;
-
-    }
-}
-
-public struct StatData
-{
-    public Stat stat;
-    public float value;
-
-    public StatData(Stat stat, float value)
-    {
-        this.stat = stat;
-        this.value = value;
-    }
-
-    public static StatData operator +(StatData data1, StatData data2)
-    {
-        if (data1.stat != data2.stat)
+        if (addends.ContainsKey(stat))
         {
-            Debug.LogError("Invalid operation, the Stats are not the same.");
-            return data1;
+            addends[stat] += value;
+        }
+        else
+        {
+            Debug.LogError($"The key {stat} has not been found, this is not normal behaviour!");
         }
 
-        float result = data1.value + data2.value;
-
-        return new StatData(data1.stat, result);
+        UpdatePlayerStats();
     }
-    public static StatData operator -(StatData data1, StatData data2)
+
+    private void UpdatePlayerStats()
     {
-        return data1 + new StatData(data2.stat, -data2.value);
+        IEnumerable<IPlayerStatsDependency> playerStatsDependencies = 
+            FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).
+            OfType<IPlayerStatsDependency>();
+
+        foreach (IPlayerStatsDependency dependency in playerStatsDependencies)
+        {
+            dependency.UpdateStats(this);
+        }
     }
 }
+
