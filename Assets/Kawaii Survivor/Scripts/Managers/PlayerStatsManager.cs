5d1ac122127838d11a77ec98072408a9901bec31
furkancanc <fcc.ceylan@gmail.com>
2025-02-06 21:39:16 +0300

    Structure Operators

--- Assets/Kawaii Survivor/Scripts/Managers/PlayerStatsManager.cs
+++ Assets/Kawaii Survivor/Scripts/Managers/PlayerStatsManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerStatsManager : MonoBehaviour
 {
     [Header("Settings")]
-    private Dictionary<Stat, float> statDatas = new Dictionary<Stat, float>();
+    private Dictionary<Stat, StatData> addens = new Dictionary<Stat, StatData>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,13 +21,46 @@ public class PlayerStatsManager : MonoBehaviour
 
     public void AddPlayerStat(Stat stat, float value)
     {
-        if (statDatas.ContainsKey(stat))
+        //if (statDatas.ContainsKey(stat))
+        //{
+        //    statDatas[stat] += value;
+        //}
+        //else
+        //{
+        //    Debug.LogError($"The key {stat} has not been found, this is not normal behaviour!");
+        //}
+
+        StatData dataToAdd = new StatData(stat, value);
+        addens[stat] += dataToAdd;
+
+    }
+}
+
+public struct StatData
+{
+    public Stat stat;
+    public float value;
+
+    public StatData(Stat stat, float value)
+    {
+        this.stat = stat;
+        this.value = value;
+    }
+
+    public static StatData operator +(StatData data1, StatData data2)
+    {
+        if (data1.stat != data2.stat)
         {
-            statDatas[stat] += value;
+            Debug.LogError("Invalid operation, the Stats are not the same.");
+            return data1;
         }
-        else
-        {
-            Debug.LogError($"The key {stat} has not been found, this is not normal behaviour!");
-        }
+
+        float result = data1.value + data2.value;
+
+        return new StatData(data1.stat, result);
+    }
+    public static StatData operator -(StatData data1, StatData data2)
+    {
+        return data1 + new StatData(data2.stat, -data2.value);
     }
 }
